772d3dcab314b9ebda451608c44c2b8387b24287
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-11 00:33:06 +0100

    simplified the binary search function (v2)

--- binary_search.go
+++ binary_search.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// This only works with sorted arrays
+// This algo only works with sorted arrays
 func binary_search(arr []int, lo int, hi int, n int) bool {
 
 	for lo < hi {
@@ -22,8 +22,27 @@ func binary_search(arr []int, lo int, hi int, n int) bool {
 	return false
 }
 
+func binary_search_v2(arr []int, n int) bool {
+
+	var lo int = 0
+	var hi int = len(arr)
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		v := arr[mid]
+
+		if v == n {
+			return true
+		} else if v < n {
+			lo = mid + 1
+		} else {
+			hi = mid
+		}
+	}
+	return false
+}
+
 func print_binary_search() {
 	var arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
-	fmt.Printf("Is the number in the array? %t", binary_search(arr, 0, len(arr), 12))
+	fmt.Printf("Is the number in the array? %t", binary_search_v2(arr, 12))
 
 }
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("vim-go")
+	print_binary_search()
 }
