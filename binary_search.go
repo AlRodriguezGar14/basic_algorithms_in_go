9469f2351edbc95214c396899bdbc67d91577da8
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-11 00:22:53 +0100

    binary search v1

--- binary_search.go
+++ binary_search.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+// This only works with sorted arrays
+func binary_search(arr []int, lo int, hi int, n int) bool {
+
+	for lo < hi {
+
+		var mid int = lo + (hi-lo)/2 // This is basically lo + half the distance to hi
+		v := arr[mid]
+
+		if v == n {
+			return true
+		} else if v < n {
+			lo = mid + 1
+		} else {
+			hi = mid
+		}
+		binary_search(arr, lo, hi, n)
+	}
+	return false
+}
+
+func print_binary_search() {
+	var arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+	fmt.Printf("Is the number in the array? %t", binary_search(arr, 0, len(arr), 12))
+
+}
