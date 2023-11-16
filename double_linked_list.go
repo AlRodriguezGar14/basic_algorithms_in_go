c04ae019f439372d7872f510798a4bf221716caa
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-16 04:01:05 +0100

    double linked list updated: added the option to remove specific value. Fixed memory access issues

--- double_linked_list.go
+++ double_linked_list.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type LNode struct {
 	Data int
@@ -30,9 +33,48 @@ func (dll *DoubleLinkedList) push(data int) {
 	dll.Head.Next.Prev = dll.Head
 }
 
+func (dll *DoubleLinkedList) remove(val int) (*LNode, error) {
+	if dll.Head == nil {
+		err := errors.New("No nodes linked")
+		return &LNode{}, err
+	}
+	idx := 0
+	curr := dll.Head
+	var removed *LNode
+	for idx < dll.Length && curr != nil {
+		if curr.Data == val {
+			removed = curr
+
+			if curr.Prev != nil {
+				curr.Prev.Next = curr.Next
+			} else {
+				dll.Head = curr.Next
+			}
+			if curr.Next != nil {
+				curr.Next.Prev = curr.Prev
+			}
+
+			dll.Length--
+			return removed, nil
+		}
+		if curr.Next != nil {
+			curr = curr.Next
+		}
+		idx++
+	}
+	return removed, nil
+}
+
 func test_ListNode() {
 	test := createSLL()
 	test.push(5)
 	test.push(60)
+	test.push(70)
+
+	fmt.Println(test.Head.Next.Data)
+	fmt.Println(test.Head.Data)
+	fmt.Println("remove")
+	test.remove(5)
+	fmt.Println(test.Head.Next.Data)
 	fmt.Println(test.Head.Data)
 }
